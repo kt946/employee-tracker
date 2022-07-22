0da78ad072b0848e25eae6d274957107ea2cc9a4
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-07-21 22:39:44 -0400

    created tables and seeds for company database

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
 const cTable = require('console.table');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'IamRoot!',
+        database: 'company'
+    },
+    console.log('Connected to the company database.')
+);
