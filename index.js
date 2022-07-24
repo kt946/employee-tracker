3c655f6e27ac7b99a01a55bf66cc66966fcf6fa3
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-07-24 12:58:58 -0400

    add function to find id and map arrays

--- index.js
+++ index.js
@@ -40,6 +40,40 @@ const menu = [
     }
 ];
 
+// function to find id
+const findId = (type, arrayData, input) => {
+    switch (type) {
+        // get id of department
+        case 'department':
+            return arrayData.find(arrayEl => arrayEl.name === input).id;
+        // get id of role
+        case 'role':
+            return arrayData.find(arrayEl => arrayEl.title === input).id;
+        // get id of employee
+        case 'employee':
+            // if employee's manager set to 'None'
+            if (input === 'None') {
+                return null;
+            }
+            return arrayData.find(arrayEl => arrayEl.first_name + ' ' + arrayEl.last_name === input).id;
+    };
+};
+
+// function to destructure and map array
+const mapArray = (type, arrayData) => {
+    switch (type) {
+        // get array of department names
+        case 'department':
+            return arrayData.map(({name}) => name);
+        // get array of role names
+        case 'role':
+            return arrayData.map(({title}) => title);
+        // get array of employee names
+        case 'employee':
+            return arrayData.map(({ first_name , last_name }) => first_name + ' ' + last_name);
+    };
+}
+
 // function to view all departments
 const viewDepartments = () => {
     const query = `SELECT * FROM departments ORDER BY id`;
@@ -168,12 +202,12 @@ const getEmployees = () => {
 // function to add a role
 const addRole = () => {
     // set up results array for department id and names
-    const resultsArray = [];
+    const departmentsArray = [];
     // get all departments, push results to array
     getDepartments()
         .then(results => {
             // push to array to find id for department after prompt
-            resultsArray.push(...results);
+            departmentsArray.push(...results);
         })
         .then(() => {
             // prompt user for role name, salary, department
@@ -200,16 +234,16 @@ const addRole = () => {
                     type: 'list',
                     name: 'department',
                     message: 'Which department does the role belong to?',
-                    // print all department names from array of objects from query
-                    choices: resultsArray.map(({name}) => name)
+                    // print all department names from array
+                    choices: mapArray('department', departmentsArray)
                 }
             ])
         })
         .then(input => {
             // get id of department
-            const id = resultsArray.find(results => results.name === input.department).id;
+            const departmentId = findId('department', departmentsArray, input.department);
             // set user input as parameters for query
-            const params = [input.role, input.salary, id];
+            const params = [input.role, input.salary, departmentId];
             const query = `INSERT INTO roles (title, salary, department_id)
                             VALUES (?,?,?)`;
             db.promise().query(query, params)
@@ -239,8 +273,8 @@ const addEmployee = () => {
         .then(getEmployees)
         .then(results => {
             managerArray.push(...results);
-            // destructure employee names and create array for manager choices
-            const managerList = results.map(({ first_name , last_name }) => first_name + ' ' + last_name);
+            // print all employee names from array for manager choices
+            const managerList = mapArray('employee', results);
             return managerList;
         })
         .then(managerList => {
@@ -276,30 +310,23 @@ const addEmployee = () => {
                     name: 'role',
                     message: "What is the employee's role?",
                     // print all role titles from array
-                    choices: roleArray.map(({title}) => title)
+                    choices: mapArray('role', roleArray)
                 },
                 {
                     type: 'list',
                     name: 'manager',
                     message: "Who is the employee's manager?",
-                    // print all employees including 'None' option
+                    // print all manager names including 'None' option
                     choices: ['None', ...managerList]
                 }
             ])
         })
         .then(input => {
-            // get id of role
-            const roleId = roleArray.find(role => role.title === input.role).id;
-            // function to get id of manager, null if 'None' is selected
-            const managerId = () => {
-                if (input.manager === 'None') {
-                    return null;
-                } else {
-                    return managerArray.find(manager => manager.first_name + ' ' + manager.last_name === input.manager).id;
-                }
-            };
+            // get ids of role and manager
+            const roleId = findId('role', roleArray, input.role);
+            const managerId = findId('employee', managerArray, input.manager);
             // set user input as parameters for query
-            const params = [input.first_name, input.last_name, roleId, managerId()]
+            const params = [input.first_name, input.last_name, roleId, managerId];
             const query = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
                             VALUES (?,?,?,?)`;
             db.promise().query(query, params)
@@ -335,22 +362,22 @@ const updateEmployee = () => {
                     type: 'list',
                     name: 'employee',
                     message: "Which employee's role do you want to update?",
-                    // destructure employee names and create array
-                    choices: employeeArray.map(({ first_name , last_name }) => first_name + ' ' + last_name)
+                    // print all employee names from array
+                    choices: mapArray('employee', employeeArray)
                 },
                 {
                     type: 'list',
                     name: 'role',
                     message: "Which role do you want to assign the selected employee?",
                     // print all role titles from array
-                    choices: roleArray.map(({title}) => title)
+                    choices: mapArray('role', roleArray)
                 },
             ])
         })
         .then(input => {
             // get ids of employee and role
-            const employeeId = employeeArray.find(employee => employee.first_name + ' ' + employee.last_name === input.employee).id;
-            const roleId = roleArray.find(role => role.title === input.role).id;
+            const employeeId = findId('employee', employeeArray, input.employee);
+            const roleId = findId('role', roleArray, input.role);
             // set user input as parameters for query
             const params = [roleId, employeeId];
             const query = `UPDATE employees SET role_id = ?
@@ -376,8 +403,8 @@ const updateManager = () => {
     getEmployees()
         .then(results => {
             employeeArray.push(...results);
-            // destructure employee names and create array for employee choices
-            const employeeList = results.map(({ first_name , last_name }) => first_name + ' ' + last_name);
+            // print all employee names from array
+            const employeeList = mapArray('employee', results);
             return employeeList;
         })
         .then(employeeList => {
@@ -392,21 +419,15 @@ const updateManager = () => {
                     type: 'list',
                     name: 'manager',
                     message: "Which manager do you want to assign the selected employee?",
-                    // add a 'None' option to manager choices
+                    // add 'None' option to manager choices
                     choices: ['None', ...employeeList]
                 },
             ])
         })
         .then(input => {
             // get ids of employee and manager
-            const findId = (arrayData, input) => {
-                if (input === 'None') {
-                    return null;
-                }
-                return arrayData.find(arrayEl => arrayEl.first_name + ' ' + arrayEl.last_name === input).id;
-            }
-            const employeeId = findId(employeeArray, input.employee);
-            const managerId = findId(employeeArray, input.manager);
+            const employeeId = findId('employee', employeeArray, input.employee);
+            const managerId = findId('employee', employeeArray, input.manager);
             const params = [managerId, employeeId];
             const query = `UPDATE employees SET manager_id = ?
                             WHERE id = ?`;
